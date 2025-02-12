a178868e54dee34bef9d2771a6f8c31cd1241b0e
damia <damianakis.p@gmail.com>
2025-02-12 17:09:22 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,147 @@
+
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using SchoolApp.Data;
+using SchoolApp.Repositories;
+using SchoolApp.Services;
+using Serilog;
+using System.Text;
+using UsersStudentsAPIApp.Helpers;
+using UsersStudentsMVCApp.Configuration;
+
+
+namespace SchoolApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Host.UseSerilog((context, config) =>
+            {
+                config.ReadFrom.Configuration(context.Configuration);
+            });
+
+            var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<SchoolAppDbContext>(options => options.UseSqlServer(connString));
+
+            builder.Services.AddScoped<IApplicationService, ApplicationService>();
+            builder.Services.AddRepositories();
+
+            builder.Services.AddScoped(provider =>
+           new MapperConfiguration(cfg =>
+           {
+               cfg.AddProfile(new MapperConfig());
+           })
+        .CreateMapper());
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                var jwtSettings = builder.Configuration.GetSection("Authentication");
+                options.IncludeErrorDetails = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidIssuer = "https://codingfactory.aueb.gr",
+
+                    ValidateAudience = false,
+                    ValidAudience = "https://api.codingfactory.aueb.gr",
+
+                    ValidateLifetime = true, // ensure not expired
+
+                    ValidateIssuerSigningKey = true,
+
+                    // US2BlUEkNFMy8yl0t6subj3cJKhAm7kQ7Asg7-mSwq0
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                    //.GetBytes("US2BlUEkNFMy8yl0t6subj3cJKhAm7kQ7Asg7-mSwq0"))
+                    .GetBytes(jwtSettings["SecretKey"]!))
+                    //.GetBytes(builder.Configuration["Authentication: SecretKey"]!))
+
+                };
+            });
+
+
+
+            // Add services to the container.
+            //cors se poio epitrepoume na xtipaei ta endpoints mas ana afiseis allow all ta afineis
+            //ola to xeirotero alla an eisaiopen API etsi prepi an omws dn eisai openAPI tote vazeis ta
+            //origin kai vazeis tin angular kai to dikoasa localhost giana mporei na mporeis kai esu apo
+            //ton upologisti na testareis 
+
+            builder.Services.AddCors(options => {
+                options.AddPolicy("AllowAll",
+                    b => b.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowAnyOrigin());
+            });
+
+            builder.Services.AddCors(options => {
+                options.AddPolicy("AngularClient",
+                    b => b.WithOrigins("http://localhost:4200") // Assuming Angular runs on localhost:4200
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+            });
+
+            builder.Services.AddControllers()
+               .AddNewtonsoftJson(options =>
+               {
+                   options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                   options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
+                   options.SerializerSettings.Converters.Add(new StringEnumConverter());
+               });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "School API", Version = "v1" });
+                // Non-nullable reference are properly documented
+                options.SupportNonNullableReferenceTypes();
+                options.OperationFilter<AuthorizeOperationFilter>();
+                options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme.",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.Http,
+                        Scheme = JwtBearerDefaults.AuthenticationScheme,
+                        BearerFormat = "JWT"
+                    });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+            app.UseCors("AllowAll");
+            app.UseAuthentication();
+
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
